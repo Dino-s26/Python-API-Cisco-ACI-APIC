e14a2c9c6f5e0941af5f88f80f426cb36a355335
Alldino Syaman <alldino.syaman@gmail.com>
2020-01-09 10:09:31 +0700

    add base file
    add base file

---
+++ get-memory.py
@@ -0,0 +1,30 @@
+import requests
+import os
+import datetime
+import json
+import list_node
+
+ddt = str(datetime.datetime.now().strftime("%d-%m-%y--%H-%M-%M"))
+
+login = "https://<IP>/api/aaaLogin.json"
+payload = "{ \"aaaUser\" : { \"attributes\": {\"name\":\"<user>\",\"pwd\":\"<pass>\"}}}"
+headers = {'Content-Type': 'application/json'}
+response = requests.request("POST", login, headers=headers, data = payload, verify=False)
+kue = response.cookies
+#print(response.cookies)
+
+for node in list_node.list_node:
+
+
+	log_name = "node"+"--"+str(node)+"--"+"<TENANT>"+"--"+"memory"+"--"+ddt+".json"
+
+	url = "https://<IP>/api/node/mo/topology/pod-1/node-"+str(node)+"/sys/procsys/HDprocSysMem5min-0.json"
+	payload  = {}
+	headers = {'Content-Type': 'application/json'}
+	response = requests.request("GET", url, headers=headers, data = payload, verify=False, cookies=kue)
+	responses = response.json()
+
+	d_path = 'C:/LOG/'
+
+	with open(d_path+log_name, 'w') as save:
+		json.dump(responses, save)
---
+++ list_node.py
@@ -0,0 +1,3 @@
+list_node = [
+<your node_list ID>
+]
